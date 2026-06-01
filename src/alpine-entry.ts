4b26a2b5f3bef89d515ef9d398d47907e4ae0db6
justinp <2763180+thatONEjustin@users.noreply.github.com>
2026-06-01 13:23:41 -0500

    updated resume and minor fixes
    i'm going to load in alpine-breakpoint even though I know the library
    needs work and isn't being used currently

--- src/alpine-entry.ts
+++ src/alpine-entry.ts
@@ -2,7 +2,6 @@ import type { Alpine } from 'alpinejs'
 import intersect from '@alpinejs/intersect'
 import swiper from '@thatonejustin/alpine-swiper' // @ts-nocheck
 
-
 export default (Alpine: Alpine) => {
     Alpine.plugin(intersect)
     Alpine.plugin(swiper)
