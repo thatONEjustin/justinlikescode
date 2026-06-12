2578170f8c6fb290c18f2a2b53711fe1a5f70edb
justinp <2763180+thatONEjustin@users.noreply.github.com>
2026-06-12 07:50:25 -0500

    contact form with hcaptcha and formspring

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -7,9 +7,9 @@ import ReCAPTCHA from "react-google-recaptcha"
 import InputField from "./InputField.tsx"
 import TextareaField from "./TextareaField.jsx"
 
-export default function FormGridForm() {
-    const FORM_SUBMIT_URL: string = ""
-    const RECAPTCHA_SITE_KEY: string = ""
+export default function ContactForm() {
+    const FORM_SUBMIT_URL: string = "https://frmsp.io/f/DvD0eexHR9D0"
+    const HCAPTCHA_SITE_KEY: string = "4a42e16a-60f4-40e4-b02e-529a268cfcbc"
 
     const [captchaValue, setCaptchaValue] = useState(null)
     const [success, setSuccess] = useState(false)
@@ -18,7 +18,7 @@ export default function FormGridForm() {
     const contactForm = useRef<HTMLFormElement>(null)
 
     async function submit(formData: FormData): Promise<void> {
-        if (!captchaValue) return
+        // if (!captchaValue) return
 
         try {
             const response = await fetch(FORM_SUBMIT_URL, {
@@ -36,78 +36,79 @@ export default function FormGridForm() {
         }
     }
 
-    function handleCaptchaChange(value: any) {
-        setCaptchaValue(value)
-    }
-
     function resetForm() {
         contactForm.current?.reset()
         setFail(false)
     }
 
     return (
-        <LayoutGroup>
-            <AnimatePresence>
-                {!success && !fail && (
-                    <motion.form
-                        className="relative"
-                        initial={{ x: "-100%" }}
-                        animate={{ x: 0, opacity: 1 }}
-                        exit={{ x: "100%", opacity: 0 }}
-                        ref={contactForm}
-                        action={submit}
-                    >
-                        <h2 className="text-4xl font-bold">Contact Me</h2>
+        <>
+            <script src="https://js.hcaptcha.com/1/api.js" async defer></script>
+            <LayoutGroup>
+                <AnimatePresence>
+                    {!success && !fail && (
+                        <motion.form
+                            className="relative"
+                            initial={{ x: "-100%" }}
+                            animate={{ x: 0, opacity: 1 }}
+                            exit={{ x: "100%", opacity: 0 }}
+                            ref={contactForm}
+                            action={submit}
+                        >
+                            <h2 className="text-4xl font-bold">Contact Me</h2>
 
-                        <fieldset>
-                            <InputField name="name" placeholder="Your Name">
-                                Name
-                            </InputField>
+                            <fieldset>
+                                <input type="text" name="_gotcha" className="hidden aria-hidden" />
 
-                            <InputField name="email" placeholder="Your Email">
-                                Email
-                            </InputField>
+                                <InputField name="name" placeholder="Your Name">
+                                    Name
+                                </InputField>
 
-                            <TextareaField name="message">
-                                Your Message
-                            </TextareaField>
-                        </fieldset>
+                                <InputField name="email" placeholder="Your Email">
+                                    Email
+                                </InputField>
 
-                        {/* <ReCAPTCHA sitekey={RECAPTCHA_SITE_KEY} onChange={handleCaptchaChange} /> */}
+                                <TextareaField name="message">
+                                    Your Message
+                                </TextareaField>
 
-                        <button type="submit" className="pill-button green mt-4">
-                            <i className="nf nf-md-send"></i>&nbsp;Contact Me!
-                        </button>
-                    </motion.form>
-                )}
+                                <div className="h-captcha" data-sitekey={HCAPTCHA_SITE_KEY}></div>
+                            </fieldset>
 
-                {success && (
-                    <motion.div
-                        className="success relative bg-transparent p-4 text-center flex items-center justify-center"
-                        style={{ height: contactForm.current?.scrollHeight }}
-                        initial={{ x: "-100%" }}
-                        animate={{ x: 0, opacity: 1 }}
-                        exit={{ x: "100%", opacity: 0 }}
-                    >
-                        <h3 className="text-2xl text-green font-bold">Thanks for contacting me!</h3>
-                    </motion.div>
-                )}
 
-                {fail && (
-                    <motion.div
-                        style={{ height: contactForm.current?.scrollHeight }}
-                        initial={{ x: "-100%" }}
-                        animate={{ x: 0, opacity: 1 }}
-                        exit={{ x: "100%", opacity: 0 }}
-                    >
-                        <div className="bg-red-600 px-4 py-2 text-center w-fit mx-auto rounded-md">
-                            There was an error submitting your form. Please try again later.
-                        </div>
-                        <button className="pill-button red mx-auto mt-4" onClick={resetForm}>Try Again</button>
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </LayoutGroup>
-    )
+                            <button type="submit" className="pill-button green mt-4">
+                                <i className="nf nf-md-send"></i>&nbsp;Contact Me!
+                            </button>
+                        </motion.form>
+                    )}
+
+                    {success && (
+                        <motion.div
+                            className="success relative bg-transparent p-4 text-center flex items-center justify-center"
+                            style={{ height: contactForm.current?.scrollHeight }}
+                            initial={{ x: "-100%" }}
+                            animate={{ x: 0, opacity: 1 }}
+                            exit={{ x: "100%", opacity: 0 }}
+                        >
+                            <h3 className="text-2xl text-green font-bold">Thanks for contacting me!</h3>
+                        </motion.div>
+                    )}
+
+                    {fail && (
+                        <motion.div
+                            style={{ height: contactForm.current?.scrollHeight }}
+                            initial={{ x: "-100%" }}
+                            animate={{ x: 0, opacity: 1 }}
+                            exit={{ x: "100%", opacity: 0 }}
+                        >
+                            <div className="bg-red-600 px-4 py-2 text-center w-fit mx-auto rounded-md">
+                                There was an error submitting your form. Please try again later.
+                            </div>
+                            <button className="pill-button red mx-auto mt-4" onClick={resetForm}>Try Again</button>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </LayoutGroup>
+        </>)
 }
 
