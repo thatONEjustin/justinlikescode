13c0471d14315c3ee3dfef00e1c8e3bb025df919
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2026-08-08 11:03:47 -0500

    theme swapping start

--- src/components/ToggleVisualTheme.tsx
+++ src/components/ToggleVisualTheme.tsx
@@ -4,17 +4,29 @@ import { useState } from "react";
 
 export default function ToggleVisualTheme(): any {
     const [theme, setTheme] = useState("light");
+    const body = document.querySelector('body');
+
+    function toggleTheme() {
+        if (theme == 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+
+        body?.classList.toggle('dark');
+    }
 
     return (
-        <div className="flex items-center justify-center">
+        <div className={`flex items-center justify-center ${theme}`}>
             <input type="hidden" name="current_theme" value={theme} />
             <label>
                 <span className="sr-only">Light/Dark Mode</span>
             </label>
 
-            <button className="relative ml-4 inline-flex w-14 rounded-full py-1 transition">
-                <span className="bg-darker-900 h-6 w-6 rounded-full transition shadow-md" aria-hidden="true">
-                    <i className={`"nf", ${theme == 'light' ? 'nf-fa-cloud_moon text-dark-700' : 'nf-fa-sun text-light-50'}`}></i>
+            <button className="relative inline-flex w-12 max-w-12 rounded-full py-1 transition-all cursor-pointer" onClick={() => toggleTheme()}>
+                <span className={`bg-darker-900 h-6 w-6 rounded-full transition-all relative ${theme == 'light' ? 'translate-x-full' : 'translate-x-0'}`} aria-hidden="true">
+                    <i className={`nf ${theme == 'light' ? 'nf-fa-sun text-light-50' : 'nf-fa-cloud_moon text-dark-700'}`}></i>
+                    {/* <i className="nf nf-fa-sun text-light-50 dark:nf-fa-cloud_moon dark:text-dark-700"></i> */}
                 </span>
             </button>
         </div>
