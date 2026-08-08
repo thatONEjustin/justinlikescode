4c86e604b8b2b64a51352718d5c1bfc0fe6bb347
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2026-08-08 10:18:11 -0500

    i guess the paths weren't saved on desktop?

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     output: 'static',
-    prefetch: false,
+    prefetch: true,
     site: 'https://staging-justinlikescode.netlify.app',
     trailingSlash: 'never',
     integrations: [mdx(), alpinejs({ entrypoint: './src/alpine-entry' }), react()],
@@ -22,7 +22,6 @@ export default defineConfig({
         }]
     },
     adapter: netlify(),
-
     vite: {
         plugins: [
             tailwindcss(),
