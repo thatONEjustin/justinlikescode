9a5e502549f56aface0f1f32ae62c317b0c608c8
thatONEjustin <2763180+thatONEjustin@users.noreply.github.com>
2026-09-03 11:22:06 -0500

    blog posts preview and proper publish dates

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,88 +1,90 @@
-import { marked } from 'marked';
-import { truncateWords } from '@shahid19/stringjs'
+import { marked } from "marked";
+import { truncateWords } from "@shahid19/stringjs";
 
 import { getData } from "@lib/helpers";
 import { defineCollection } from "astro:content";
 
 function preview(text: string, truncate: number = 0) {
     if (truncate >= 1 && truncate != null) {
-        text = truncateWords(text, truncate)
+        text = truncateWords(text, truncate);
     }
 
-    const marked_down_text = marked.parse(text) as string
-    const cleaned_text = marked_down_text.replace(/<\/?[^>]+(>|$)/g, "")
+    const marked_down_text = marked.parse(text) as string;
+    const cleaned_text = marked_down_text.replace(/<\/?[^>]+(>|$)/g, "");
 
-    return cleaned_text
+    return cleaned_text;
 }
-
 
 const menu_items = defineCollection({
     loader: async (): Promise<any> => {
-
         // const menuItemsJson: MenuItemType[] = [
         const menuItemsJson = [
             /*{
-                label: 'home',
-                href: '/',
-                icon: 'nf-custom-home',
-            },*/
+                                  label: 'home',
+                                  href: '/',
+                                  icon: 'nf-custom-home',
+                              },*/
             {
                 id: "1",
-                label: 'about me',
-                href: '/about-me',
-                icon: 'nf-oct-person_fill',
+                label: "about me",
+                href: "/about-me",
+                icon: "nf-oct-person_fill",
             },
             {
                 id: "2",
-                label: 'portfolio',
-                href: '/portfolio',
-                icon: 'nf-dev-terminal'
+                label: "portfolio",
+                href: "/portfolio",
+                icon: "nf-dev-terminal",
             },
             {
                 id: "3",
-                label: 'contact me',
-                href: '/contact-me',
-                icon: 'nf-oct-mail',
-            }
-        ]
+                label: "contact me",
+                href: "/contact-me",
+                icon: "nf-oct-mail",
+            },
+        ];
 
-        return menuItemsJson
-    }
-})
+        return menuItemsJson;
+    },
+});
 
 const blog_posts = defineCollection({
     loader: async () => {
-        const data = await getData('blog-posts').then(r => r.json())
+        const data = await getData("blog-posts").then((r) => r.json());
 
-        if (data.data == null) return {} // if no data then return empty
+        if (data.data == null) return {}; // if no data then return empty
 
-        const posts = data.data
+        const posts = data.data;
 
         return posts.map(({ ...post }: any) => {
-            const { documentId, slug, publishedAt, Content, Title } = post
+            const { documentId, slug, publishedAt, createdAt, Preview, Content, Title } = post;
             return {
                 id: documentId,
                 slug: slug,
                 content: Content,
                 publishedAt: publishedAt,
-                preview: truncateWords(Content, 60),
-                title: Title
-            }
-        })
+                createdAt: createdAt,
+                preview: Preview,
+                title: Title,
+            };
+        });
     },
 });
 
 const projects = defineCollection({
     loader: async () => {
-        const data = await getData('projects?populate=Technology&populate=screenshots&sort=category').then(response => response.json())
+        const data = await getData(
+            "projects?populate=Technology&populate=screenshots&sort=category",
+        ).then((response) => response.json());
         // console.log(data)
-        if (data.data == null) return {}
+        if (data.data == null) return {};
 
         return data.data.map(({ ...project }: any) => {
             // NOTE: this will probably return weird data for now
-            const { slug, preview, category, description, external_url, Title, updatedAt, Technology } = project
+            const { slug, preview, category, description, external_url, Title, updatedAt, Technology } =
+                project;
 
-            let screenshots = (project.screenshots != null) ? project.screenshots : [];
+            let screenshots = project.screenshots != null ? project.screenshots : [];
 
             return {
                 id: project.documentId,
@@ -94,14 +96,14 @@ const projects = defineCollection({
                 screenshots: screenshots,
                 updatedAt: updatedAt,
                 category: category,
-                techStack: Technology
-            }
-        })
+                techStack: Technology,
+            };
+        });
     },
-})
+});
 
 export const collections = {
-    "blog": blog_posts,
-    "projects": projects,
-    "menuItems": menu_items
+    blog: blog_posts,
+    projects: projects,
+    menuItems: menu_items,
 };
